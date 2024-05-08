1b0c735ebd48fba32496643c322ebbc889456ff3
4DERT <4dertdk@gmail.com>
2024-05-08 13:09:23 +0200

    Refactor main

--- main/main.c
+++ main/main.c
@@ -8,25 +8,12 @@
 // FreeRTOS
 #include "FreeRTOSConfig.h"
 #include "freertos/FreeRTOS.h"
-#include "freertos/semphr.h"
-#include "freertos/task.h"
 #include "portmacro.h"
 
 // ESP
-#include "driver/gpio.h"
 #include "driver/uart.h"
-#include "errno.h"
-#include "esp_err.h"
-#include "esp_event.h"
-#include "esp_log.h"
-#include "esp_netif.h"
-#include "esp_now.h"
-#include "esp_spi_flash.h"
-#include "esp_system.h"
-#include "esp_wifi.h"
 #include "nvs.h"
 #include "nvs_flash.h"
-#include "tcpip_adapter.h"
 
 // project includes
 #include "esp-now-communication.h"
@@ -38,14 +25,10 @@
 void app_main() {
   uart_set_baudrate(0, 115200);
   nvs_flash_init();
-  esp_now_communication_init();
   leds_init();
   relay_init();
   button_init();
 
+  esp_now_communication_init();
   init_gateway_mac(button_is_pressed());
-
-  while (1) {
-    vTaskDelay(pdMS_TO_TICKS(100));
-  }
 }
